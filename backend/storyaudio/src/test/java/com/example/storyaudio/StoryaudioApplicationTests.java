9fa3e9d2abf74cf658eafc8ca0666e51710f37b3
shreyas <jojoleo507@gmail.com>
2025-07-31 20:20:12 +0530

    Initial commit: setup backend and frontend

---
+++ backend/storyaudio/src/test/java/com/example/storyaudio/StoryaudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storyaudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryaudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
